5354cb2a795b28403a3c649a21b0c9a4909fb141
dianzzz <lxr784859747@gmail.com>
2025-02-05 10:54:19 +0800

    delete chromium version when kill

--- run_daily_docker.sh
+++ run_daily_docker.sh
@@ -3,7 +3,7 @@
 # Set up environment variables
 export PATH=$PATH:/usr/local/bin:/usr/bin:/bin:/usr/local/sbin:/usr/sbin:/sbin
 
-ps -aux | grep "chromium-1140\|npm" | grep -v "grep\|sh" | awk '{print $2}' | xargs kill -9 2>/dev/null
+ps -aux | grep "chromium\|npm" | grep -v "grep\|sh" | awk '{print $2}' | xargs kill -9 2>/dev/null
 
 /usr/bin/systemctl restart v2raya.service
 
